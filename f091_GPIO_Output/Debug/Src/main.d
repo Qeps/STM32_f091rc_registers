17b2dc334980cd5e03ba502472e39851f2bd07f1
GerBor <gerard.boruszewicz@gmail.com>
2025-10-21 23:17:27 +0200

    Diode blink program using stm32 registers (without HAL)

---
+++ f091_GPIO_Output/Debug/Src/main.d
@@ -0,0 +1,16 @@
+Src/main.o: ../Src/main.c ../Inc/main.h \
+ ../Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h \
+ ../Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f091xc.h \
+ ../Drivers/CMSIS/Include/core_cm0.h \
+ ../Drivers/CMSIS/Include/cmsis_version.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../Drivers/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h
+../Inc/main.h:
+../Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h:
+../Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f091xc.h:
+../Drivers/CMSIS/Include/core_cm0.h:
+../Drivers/CMSIS/Include/cmsis_version.h:
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+../Drivers/CMSIS/Include/cmsis_gcc.h:
+../Drivers/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h:
