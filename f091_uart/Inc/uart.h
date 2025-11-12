e81c7d3525844da476702cb96c1cc3b7c14b1bfa
GerBor <gerard.boruszewicz@gmail.com>
2025-11-12 20:04:41 +0100

    UART2 init

---
+++ f091_uart/Inc/uart.h
@@ -0,0 +1,14 @@
+/*
+ * uart.h
+ *
+ *  Created on: Nov 12, 2025
+ *      Author: lenovo
+ */
+
+#ifndef UART_H_
+#define UART_H_
+
+#include <stdint.h>
+#include "stm32f0xx.h"
+
+#endif /* UART_H_ */
---
+++ f091_uart/Src/uart.c
@@ -0,0 +1,58 @@
+/*
+ * uart.c
+ *
+ *  Created on: Nov 12, 2025
+ *      Author: lenovo
+ */
+
+#include <uart.h>
+
+#define UART_BAUDRATE 115200
+#define CLK			  8000000
+static uint16_t compute_uart_baudrate(uint32_t periph_clk, uint32_t baudrate);
+static void uart_set_baudrate(uint32_t periph_clk, uint32_t baudrate);
+
+void uart2_init(void){
+	// 1. Enable clock signal for GPIOA
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+
+	// 2. Set PA2 mode to alternate function mode (10)
+	GPIOA->MODER |= GPIO_MODER_MODER2_1;
+	GPIOA->MODER &= ~GPIO_MODER_MODER2_0;
+
+	// 3. Set PA3 mode to alternate function mode
+	GPIOA->MODER |= GPIO_MODER_MODER3_1;
+	GPIOA->MODER &= ~GPIO_MODER_MODER3_0;
+
+	// 4. Set PA2 alternate function type to AF1(UART2_TX) - GPIOx_AFRL 0001
+	GPIOA->AFR[0] &= ~GPIO_AFRL_AFSEL2_Msk;			// clear all 4 bitfields
+	GPIOA->AFR[0] |= (1U << GPIO_AFRL_AFSEL2_Pos);	// set 0001 value
+
+	// 5. Set PA3 alternate function type to AF1(UART2_RX)
+	GPIOA->AFR[0] &= ~GPIO_AFRL_AFSEL3_Msk;			// clear all 4 bitfields
+	GPIOA->AFR[0] |= (1U << GPIO_AFRL_AFSEL3_Pos);	// set 0001 value
+
+	// 6. Enable clock signal for USART2 on APB bus (17th bit)
+	RCC->APB1ENR |= RCC_APB1ENR_USART2EN;
+
+	// 7. Set baudrate
+	uart_set_baudrate(CLK, UART_BAUDRATE);
+
+	// 8. Set transfer direction
+	USART2->CR1 = 0;
+	USART2->CR1 |= USART_CR1_TE; // Bit 2 RE: Receiver enable
+	USART2->CR1 |= USART_CR1_RE; // Bit 3 TE: Transmitter enable
+
+	// 9. Enable UART module
+	USART2->CR1 |= USART_CR1_UE;
+}
+
+static uint16_t compute_uart_baudrate(uint32_t periph_clk, uint32_t baudrate){
+	return ((periph_clk + (baudrate/2U))/baudrate);
+}
+
+static void uart_set_baudrate(uint32_t periph_clk, uint32_t baudrate){
+	USART2->BRR = compute_uart_baudrate(periph_clk, baudrate);
+}
+
+
