4fe3a2eb43532dd0e1a1e47eb8c8c29e3c034b27
GerBor <gerard.boruszewicz@gmail.com>
2025-11-16 20:03:08 +0100

    Missing ';' added

--- f091_ADC_DMA/Src/adc.c
+++ f091_ADC_DMA/Src/adc.c
@@ -7,11 +7,12 @@
 
 #include "adc.h"
 
-void adc_dma_init(void){
+void adc_dma_init(adc_resolution_t res, adc_scan_dir_t scan, uint32_t channels_mask){
 
 	//1. GPIO config
 	//	a. Enable clock access to ADC GPIO Pin's port
 	RCC->AHBENR 	|= RCC_AHBENR_GPIOAEN;
+
 	//	b. Set PA0 and PA1 mode to analog
 	GPIOA->MODER 	|= GPIO_MODER_MODER0_0 | GPIO_MODER_MODER0_1;
 	GPIOA->MODER 	|= GPIO_MODER_MODER1_0 | GPIO_MODER_MODER1_1;
@@ -19,13 +20,22 @@ void adc_dma_init(void){
 	//2. ADC config
 	//	a. Enable clock access to ADC
 	RCC->APB2ENR 	|= RCC_APB2ENR_ADCEN;
+
 	//	b. Select channels and set sequence (which pin will be first)
-	ADC1->CHSELR 	|= ADC_CHSELR_CHSEL0 | ADC_CHSELR_CHSEL1;
-	ADC1->CFGR1 	&= ~ADC_CFGR1_SCANDIR; //ch0->ch1->...
-	//	c. Select continuous coversion
-	ADC1->CFGR1		|= ADC_CFGR1_CONT;
-	//	d. Select to use DMA
-	ADC1->CFGR1		|= ADC_CFGR1_DMAEN | ADC_CFGR1_DMACFG;
+	ADC1->CHSELR 	|= channels_mask;
+
+	// 	c. Configure resolution + scan direction
+	ADC1->CFGR1 	&= ~(ADC_CFGR1_RES | ADC_CFGR1_SCANDIR);
+	ADC1->CFGR1		|= (res | scan);
+
+	//  d. ADC calibration
+	ADC1->CR |= ADC_CR_ADCAL;
+	while (ADC1->CR & ADC_CR_ADCAL){
+		//Time-out managment
+	}
+
+	//	e. Select continuous coversion and to use DMA
+	ADC1->CFGR1		|= ADC_CFGR1_CONT | ADC_CFGR1_DMAEN | ADC_CFGR1_DMACFG;
 
 	//3. DMA config
 	//	a. Enable clock access to DMA
--- f091_ADC_DMA/Src/main.c
+++ f091_ADC_DMA/Src/main.c
@@ -11,30 +11,3 @@ int main(void)
 
 	}
 }
-
-void DMA1_CH1_IRQHandler(void){
-	// Handle interrupt:
-	// DMA_ISR	- DMA interrupt status register
-	// DMA_IFCR	- DMA interrupt flag clear register
-	// TCIF1	- transfer complete (TC) flag for channel 1
-	// CTCIF1	- transfer complete flag clear for channel 1
-	// TEIF1	- transfer error (TE) flag for channel 1
-	// CTEIF1	- transfer error flag clear for channel 1
-
-	// 1. Check if transfer complete flag is set to 1
-    if (DMA1->ISR & DMA_ISR_TCIF1){
-
-    // 2. Set flag for main loop to perform printing results
-        dma1_m2m_transf_cmplt = 1;
-
-    // 3. Set clear flag for coresponding register
-        DMA1->IFCR |= DMA_IFCR_CTCIF1;
-    }
-
-    // 4. Check if transfer error flag occured
-    if (DMA1->ISR & DMA_ISR_TEIF1) {
-
-    // 5. Set clear flag for coresponding error register
-        DMA1->IFCR |= DMA_IFCR_CTEIF1;
-    }
-}
